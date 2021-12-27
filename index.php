7b681859a0c1e8104bbc61ac63a8b47906af656c
Md. Talat Morshed <hossaintanbir0@gmail.com>
2021-12-27 11:31:41 +0600

    Add code

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+// Comment these lines to hide errors
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+
+require 'includes/config.php';
+require 'includes/functions.php';
+//This Change is made by Zafar
+//Assignment 01 by Zafar
+//Changed by Tanvir_Assignment_01
+
+//This Change is made by Md. Talat Morshed
+//Assignment 01
+
+init();
